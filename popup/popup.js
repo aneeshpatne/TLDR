47b4ef470cc6b3c522f03f30b9176ce877ee5d3c
aneeshpatne <aneeshpatne@gmail.com>
2025-01-09 12:56:13 +0530

    refactor: main logic refined

--- popup/popup.js
+++ popup/popup.js
@@ -1,30 +1,33 @@
-document.getElementById("summarize-btn").addEventListener("click", async () => {
-    try {
-      const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-      data = await chrome.scripting.executeScript({
-        target: { tabId: tab.id},
-        func: () => {
-          return new Promise((resolve, reject) => {
-            setTimeout(() => {
-              const body = document.body.innerText;
-              resolve(body || "No content found");
-            }, 1000);
-          })
-        }
-      });
-    
-    const response = await fetch("http://127.0.0.1:5000/extract", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({text: data[0].result}),
+document.getElementById('summarize-btn').addEventListener('click', async () =>{
+  let summarybox =document.getElementById('summary-box')
+  let summaryText = document.getElementById('summary-text')
+  try{
+    summarybox.style.display = 'block';
+    summaryText.innerText = 'Loading...';
+    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    const url = tab.url;
+    const [HTMLcontent] = await chrome.scripting.executeScript({
+      target: {tabId: tab.id},
+      function: () => {
+        return document.body.innerText;
+      }
     })
-    data = await response.json();
-    document.getElementById("summary-box").style.display = "flex";
-    document.getElementById("summary-text").innerText = data.message;
-    } catch (error) {
-      console.error(error);
-    }
+    const response = await fetch("http://127.0.0.1:5000/extract", {
+      method: 'POST',
+      headers:{
+        'Content-Type': 'application/json'
+      },
+      body :JSON.stringify({text: HTMLcontent.result})
+    })
+    const data = await response.json();
+    document.getElementById('summarize-btn').style.display = 'none';
+    summaryText.innerText = data.message;
 
-  });
+  
+
+  }
+  catch(error){
+    console.error(error);
+    summaryText.innerText = 'An error occured while summarizing the text';
+  }
+})
